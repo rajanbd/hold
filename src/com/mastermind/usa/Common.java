c1a6d8c21130a87e0f57e1e2e87b0a977ec7cc63
rajanbd <rajan.bd.usa@gmail.com>
2022-04-14 07:17:45 -0700

    new music

---
+++ src/com/mastermind/usa/Common.java
@@ -0,0 +1,31 @@
+package com.mastermind.usa;
+
+public class Common {
+
+	public static void main(String[] args) {
+		//int a;
+		//a=10;
+		
+		int a=10;
+		
+		
+		
+		int b=5;
+		
+		
+		
+		int c=6;
+		
+		
+		int g=a+b+c;
+		
+		
+		System.out.println(g);
+		
+		
+	}
+	
+	
+	
+
+}
